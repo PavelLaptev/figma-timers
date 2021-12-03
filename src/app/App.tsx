ae6d786ecdf513bae635a690e78c2a2b6370c172
LaptevPavel <laptev.graphics@gmail.com>
2021-12-03 13:22:38 +0100

    ff zustand

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import useStore from "./useStore";
-
 import styles from "./app.module.scss";
+
+import Timer from "./components/Timer";
 
 console.clear();
 
@@ -25,7 +26,7 @@ const App = ({}) => {
       </section>
       <section className={styles.timersList}>
         {config.map((_, index) => {
-          return <div>{index}</div>;
+          return <Timer key={`timer-${index}`} index={index} />;
         })}
       </section>
     </div>
