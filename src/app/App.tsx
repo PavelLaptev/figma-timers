20706d9cb5d54bf5fdfd35ace946f5cd27afeeda
LaptevPavel <laptev.graphics@gmail.com>
2021-12-03 11:55:40 +0100

    wip

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,42 +1,70 @@
 import * as React from "react";
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue
+} from "recoil";
 
 import Timer from "./components/Timer";
 import styles from "./app.module.scss";
 
 console.clear();
 
-const timerConfig = [
-  {
-    name: "Intro",
-    time: {
-      minutes: 0,
-      seconds: 3
+const config = atom({
+  key: "config", // unique ID (with respect to other atoms/selectors)
+  default: [
+    {
+      name: "Intro",
+      time: {
+        minutes: 0,
+        seconds: 3
+      },
+      skip: false
     },
-    skip: false
-  },
-  {
-    name: "Goals",
-    time: {
-      minutes: 0,
-      seconds: 7
+    {
+      name: "Goals",
+      time: {
+        minutes: 0,
+        seconds: 7
+      },
+      skip: false
     },
-    skip: false
-  },
-  {
-    name: "Sketching",
-    time: {
-      minutes: 0,
-      seconds: 2
-    },
-    skip: false
-  }
-] as Array<TimerConfigProps>;
+    {
+      name: "Sketching",
+      time: {
+        minutes: 0,
+        seconds: 2
+      },
+      skip: false
+    }
+  ] as Array<TimerConfigProps> // default value (aka initial value)
+});
 
 const App = ({}) => {
+  const handlePlay = () => {
+    // setPlayState(true);
+  };
+
+  const handlePause = () => {
+    // setPlayState(false);
+  };
+
   return (
-    <section className={styles.app}>
-      <Timer timerConfig={timerConfig} />
-    </section>
+    <RecoilRoot>
+      <div>
+        <section className={styles.app}>
+          <button onClick={handlePlay}>Play</button>
+          <button onClick={handlePause}>Pause</button>
+        </section>
+        <section className={styles.timersList}>
+          {/* {mainProps.timerConfig.map((_, index) => {
+          return <TimerItem index={index} key={`timer-${index}`} />;
+        })} */}
+        </section>
+      </div>
+    </RecoilRoot>
   );
 };
 
